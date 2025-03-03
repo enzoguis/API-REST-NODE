60e539d3ef2b711a76c7822c95c7fc566ea8bf38
enzoguis <enzog0315@gmail.com>
2025-03-03 15:27:05 -0300

    feat(routes): adding Fastify plugin to init my routes implementation

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
 import fastify from 'fastify'
 import { knex } from './database'
-import { randomUUID } from 'crypto'
+import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transactions = await knex('transactions').select('*')
-
-  return transactions
-})
-app.listen({ port: 3333 }).then(() => {
+app.register(transactionsRoutes)
+app.listen({ port: env.PORT }).then(() => {
   console.log('Server running on port 3333')
 })
