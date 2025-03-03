173abe46cdd2f652819c43c5a5a2165b1e500324
enzoguis <enzog0315@gmail.com>
2025-03-03 16:26:04 -0300

    feat(routes): adding sessionId with cookies

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -28,10 +28,22 @@ export async function transactionsRoutes(app: FastifyInstance) {
   app.post('/', async (request, reply) => {
     const { title, amount, type } = createTransactionSchema.parse(request.body)
 
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days,
+      })
+    }
+
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     return reply.status(201).send()
