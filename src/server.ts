10b8e14b848d78489cb6a65ddf17b0867abc3e0c
enzoguis <enzog0315@gmail.com>
2025-03-03 15:50:12 -0300

    feat(knex-types): adding file to modificate the knex Tables interface

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,6 @@ const app = fastify()
 app.register(transactionsRoutes, {
   prefix: '/transactions',
 })
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log('Server running on port 3333')
 })
