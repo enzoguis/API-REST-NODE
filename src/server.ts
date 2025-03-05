c1e932a15af14f67b288bd6932f0f0f9e2343d81
enzoguis <enzog0315@gmail.com>
2025-03-05 13:21:07 -0300

    fix(app-listen): fixing the undefined port

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { app } from './app'
 import { env } from './env'
-app.listen({ port: env.PORT }).then(() => {
+app.listen({ port: env.PORT || 3000 }).then(() => {
   console.log('Server running on port: ', env.PORT)
 })
