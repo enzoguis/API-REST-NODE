ff8f23d75762a3e89ddec0e5b137b4d10ed10df5
enzoguis <enzog0315@gmail.com>
2025-03-05 14:28:14 -0300

    fix(list-transaction): fixing the session_id not found error

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -10,7 +10,7 @@ export async function transactionsRoutes(app: FastifyInstance) {
     const { sessionId } = request.cookies
 
     const transactions = await knex('transactions')
-      .where('session_Id', sessionId)
+      .where('session_id', sessionId)
       .select()
 
     return { transactions }
